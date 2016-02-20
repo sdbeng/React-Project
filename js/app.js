28b5ceffc425f831e83e3f0ccc574e1e14f44a82
Sergio Barrera <sdbeng@gmail.com>
2016-02-19 23:48:28 -0800

    Add handleClick function and show off number of clicks

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
-
+//don't forget the commas between functions 
 
 var Counter = React.createClass({
+	getInitialState: function(){
+		return {clickCount: 0}
+	},
+	handleClick: function(){
+		this.setState(function(state){
+			return {clickCount: state.clickCount + 1}
+		})
+	},
+
+
 	render:function(){
-		return (<h2> -hello Counter- </h2>);
+		return (
+			//in order to show more than 1 element, they must be wrapped in a div
+			<div>
+			<h1>Hello Counter - DDD</h1>
+			<button onClick={this.handleClick}> Click me!</button>
+			<p> Number of clicks: {this.state.clickCount} </p>
+			</div>
+		);
 	}
 });
 
-React.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
